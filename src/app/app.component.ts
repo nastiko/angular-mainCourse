7348fc42cd2947d95c99fa40d636e475830d8269
Anastasia H. <anastasia.hrynkevich@outlook.com>
2025-04-09 21:43:46 +0100

    created components: "header" and "home"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <app-header />
+    <main>
+      <app-home />
+    </main>
 
-    <router-outlet />
   `,
-  styles: [],
+  styles: [
+    `
+      main {
+        padding: 0 25px;
+      }
+    `
+  ],
 })
 export class AppComponent {
   title = 'angular-mainCourse';
